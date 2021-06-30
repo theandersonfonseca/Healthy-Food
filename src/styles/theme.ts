d2ec711dfe804f0c357aec3d2e961a88d0a8cf04
Anderson <andersonfonseca1999@hotmail.com>
2021-06-30 20:12:03 -0300

    Add initial configuration

---
+++ src/styles/theme.ts
@@ -0,0 +1,48 @@
+export default {
+  border: {
+    radius: '0.4rem'
+  },
+  font: {
+    family:
+      "Montserrat, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    normal: 400,
+    bold: 700,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem',
+      huge: '5.2rem'
+    }
+  },
+  colors: {
+    primary: '#BADC58',
+    secondary: '#1D164D',
+    white: '#FFFFFF',
+    lightGray: '#FAFAFC',
+    text: '#707070',
+    red: '#FF6347'
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  },
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out'
+  }
+} as const
