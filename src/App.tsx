55aeed35dd225e1335c6ad1f47447fac3fe289d9
Anderson <andersonfonseca1999@hotmail.com>
2021-07-03 17:20:02 -0300

    Add App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components';
 
-import GlobalStyles from './styles/global'
-import theme from './styles/theme'
+import GlobalStyles from './styles/global';
+import theme from './styles/theme';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div >
-        <button style={{background: '#1D164D', color: '#fff', padding: '20px'}}>teste</button>
-      </div>
-      <GlobalStyles/>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+      <GlobalStyles />
     </ThemeProvider>
-
   );
 }
 
